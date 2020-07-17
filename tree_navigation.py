3c526124e49e862fda94bf814b659f9acb9e5612
Dave Yoon <daveyoon64@gmail.com>
2020-07-16 21:31:08 -0700

    Experimenting with traversing the BeautifulSoup object

---
+++ tree_navigation.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bs = BeautifulSoup(html, "html.parser")
+
+#for child in bs.find('table', {'id':'giftList'}).children:
+#    print(child)
+
+#for sibling in bs.find('table', {'id':'giftList'}).tr.next_siblings:
+#    print(sibling)
+
+
+
+for sibling in bs.find('table', {'id':'giftList'}).descendants:
+    print(sibling)
